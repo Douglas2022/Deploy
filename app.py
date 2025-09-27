aa5708001ad55272f3ab62702d099c85f849f0fb
Douglas Silva <paulodnunes2016@gmail.com>
2025-09-27 20:00:52 -0300

    Aula dia 27 - deploy

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,url_for
+import mysql.connector as my
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+user = os.getenv('USER')
+password = os.getenv('PASSWORD')
+database = os.getenv('DATABASE')
+host = os.getenv('HOST')
 
 app = Flask(__name__)
 
-@app.route('/')
+
+
+def connectorBanco():
+    conexao = my.connect(
+        user=user,
+        password=password,
+        database=database,
+        host=host
+    )
+    return conexao
+
+
+@app.route('/',methods=['GET','POST'])
 def index():
     titulo = 'Página Inicial'
+    if request.method == 'POST':
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        print(f'Email:{email},senha:{senha}')
+
+        return render_template('index.html',titulo=titulo)
+    
+    
+
     return render_template('index.html',titulo=titulo)
+    
 
 
 if __name__ == '__main__':
